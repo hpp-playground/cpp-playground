2bbca2193ad2f02c5e0d9524b95b54af15106eff
hppRC <hpp.ricecake@gmail.com>
2020-08-23 17:01:46 +0900

    🥺chore

---
+++ src/array.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(void) {
+  int N = 20;
+  int array[N];
+
+  for (int i = 0; i < N; i++) {
+    printf("%d\n", array[i]);
+  }
+
+  return 0;
+}
